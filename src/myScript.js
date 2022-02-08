a96c1d05d747cce45c673dd3c5afe1f8f7192937
shaheer@264 <shaheermustafahashmi@cedcoss.com>
2022-02-08 12:19:11 +0530

    Errors fixed

--- src/myScript.js
+++ src/myScript.js
@@ -1,29 +1,44 @@
+arr=[];
+
 function myFunction () {
     var id = document.getElementById('ID').value;
     var price = document.getElementById('Price').value;
     var name = document.getElementById('Name').value;
-    var output= {};
     console.log("correct");
-    output = logic (id, price, name);
-    display(output);
+    logic (id, price, name);
+    display(arr);
 }
-function logic(id, price, name) {
-    arr=[];
-    obj = {};
+
+function logic(id, price, name) 
+{
+    for (var i = 0; i < arr.length; i++)
     {
-        obj["id"] = id;
-        obj["name"] = name;
-        obj["price"] = price; 
-        arr.push(obj);
-        console.log(arr);
-        return arr;  
+        console.log(arr[i]);
+        if (arr[i].id == id)
+        {
+            alert("Product ID already exists");
+            return;
+        }
     }
+    obj = {};
+    obj["id"] = id;
+    obj["name"] = name;
+    obj["Price"] = price; 
+    arr.push(obj);
 }
+
 function display(result) {
     var text=document.getElementById("table1");
-    text.innerHTML+=`<tr> 
-    <td>${result[0].id}</td>
-    <td>${result[0].name}</td>
-    <td>${result[0].price}</td>
- </tr>`;
-}  
+    text.innerHTML="<tr>\
+    <td>Product Id</td>\
+    <td>Product Name</td>\
+    <td>Product Price</td>\
+    </tr>";
+    for (let i=0; i<result.length; i++) {
+        text.innerHTML+='<tr>\
+        <td>' + result[i].id + '</td>\
+        <td>' + result[i].name + '</td>\
+        <td>' + result[i].Price + '</td>\
+    </tr>';
+    }
+}
